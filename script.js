85d37d89f2376d521cdfd3522f63cb8b52a6f254
Renato_Alencar <renato.augustoalencar@gmail.com>
2020-10-08 20:56:12 -0300

    Parte6: Adiciona a funcionalidade de mudança de jogadore inicia funcionalidade para verificar o vencedor de acordo com a sequência de IDs.

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 var player = null;
-var playerSelected = document.getElementById(elementid = 'player-selected');
+var playerSelected = document.getElementById(elementId = 'player-selected');
+var squares = document.getElementsByClassName(className = 'square');
 
 changePlayer(value = 'X');
 
 function chooseSquare(id) {
     var square = document.getElementById(id);
+    if(square.innerHTML !== '-') {
+        return;
+    }
 
     square.innerHTML = player;
     square.style.color = '#000';
+
+    if(player === 'X') {
+        player = 'O';
+    }
+    else {
+        player = 'X';
+    }
+    changePlayer(player);
 }
 
 function changePlayer(value) {
     player = value;
     playerSelected.innerHTML = player;
+}
+
+function checkWinner() {
+    var square1 = document.getElementById(elementId = 1);
+    var square2 = document.getElementById(elementId = 2);
+    var square3 = document.getElementById(elementId = 3);
+    var square4 = document.getElementById(elementId = 4);
+    var square5 = document.getElementById(elementId = 5);
+    var square6 = document.getElementById(elementId = 6);
+    var square7 = document.getElementById(elementId = 7);
+    var square8 = document.getElementById(elementId = 8);
+    var square9 = document.getElementById(elementId = 9);
+}
+
+function checkSequence() {
+    
 }
