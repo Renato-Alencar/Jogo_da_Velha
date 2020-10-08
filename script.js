ca69f754105a8609cd761b163586c051a4e38a5d
Renato_Alencar <renato.augustoalencar@gmail.com>
2020-10-08 20:08:58 -0300

    Parte5: Adiciona a funcionalidade chooseSquare que mostra o id, o player e adiciona a cor preta. E adiciona changePlayer.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var player = null;
+var playerSelected = document.getElementById(elementid = 'player-selected');
+
+changePlayer(value = 'X');
 
 function chooseSquare(id) {
-    console.log('ID: ', id);
+    var square = document.getElementById(id);
+
+    square.innerHTML = player;
+    square.style.color = '#000';
+}
+
+function changePlayer(value) {
+    player = value;
+    playerSelected.innerHTML = player;
 }
